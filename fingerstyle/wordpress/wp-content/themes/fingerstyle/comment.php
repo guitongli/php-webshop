d5428d07c066fc36ddac3c2b29bbfbf8140288ca
guitongli <guitong.lee.contact@gmail.com>
2021-06-06 23:07:13 +0200

    comment section in progress

---
+++ fingerstyle/wordpress/wp-content/themes/fingerstyle/comment.php
@@ -0,0 +1,22 @@
+<?php
+if(!have_comments()){
+   echo 'leave a comment';
+} else {
+   echo get_comments_count().'Comments';
+} ?>
+
+<?php
+wp_list_comments(array('avatar_size'=>120,
+'style'=>'div'
+));?>
+
+<?php 
+if(comments_open()){
+comment_form(
+    array(
+'class_form'=>'',
+'title_reply_before'=>'<h2 id="reply-title">',
+'title_reply_after'=>'</h2>'
+    )
+    );
+}?>
